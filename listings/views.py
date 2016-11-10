d5097b8f75cc765402443c30a524ed5a4a35eeb0
Adam McCartney <adam.mccartney@gmail.com>
2016-11-10 23:14:13 +0000

    Got Started with Listing Submission form

--- listings/views.py
+++ listings/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from .models import Listing
 from .forms import ListingForm
 
+
 def index(request):
     return HttpResponse("Hello, world. You're at the Listings index.")
 
@@ -18,5 +19,18 @@ def fallen_through(request):
 
 
 def listing_new(request):
-    form = ListingForm()
+    if request.method == "POST":
+        form = ListingForm(request.POST)
+        if form.is_valid():
+            listing = form.save(commit=False)
+            listing.team_member = request.team_member
+            listing.address = request.address
+            listing.list_price = request.list_price
+            listing.commission_rate = request.commission_rate
+            listing.commission_value = request.commission_value
+
+            listing.save()
+            # return redirect('sales')
+        else:
+            form = ListingFormForm()
     return render(request, 'listings/listing_new.html', {'form': form})
